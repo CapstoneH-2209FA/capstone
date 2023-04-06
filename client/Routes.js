2452e5df6911cf805c450d891420a54ad6308f88
Jerral Graham <jerralgraham@yahoo.com>
2023-04-06 11:28:06 -0400

    HashRouter

--- client/Routes.js
+++ client/Routes.js
@@ -1,6 +1,12 @@
 import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import {
+  withRouter,
+  Route,
+  Switch,
+  Redirect,
+  HashRouter,
+} from "react-router-dom";
 import history from "./history";
 import { Login, Signup } from "./components/AuthForm";
 import Home from "./components/Home";
@@ -25,21 +31,25 @@ class Routes extends Component {
     return (
       <div>
         {isLoggedIn ? (
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/projects/:projectId" component={Project} />
-            <Route path="/Whiteboard/:projectId" component={Whiteboard} />
-            <Route path="/chat" component={Chat} />
-            <Redirect to="/home" />
-          </Switch>
+          <HashRouter>
+            <Switch>
+              <Route path="/home" component={Home} />
+              <Route path="/projects/:projectId" component={Project} />
+              <Route path="/Whiteboard/:projectId" component={Whiteboard} />
+              <Route path="/chat" component={Chat} />
+              <Redirect to="/home" />
+            </Switch>
+          </HashRouter>
         ) : (
-          <Switch>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
+          <HashRouter>
+            <Switch>
+              <Route path="/" exact component={LandingPage} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
 
-            <Redirect to="/" />
-          </Switch>
+              <Redirect to="/" />
+            </Switch>
+          </HashRouter>
         )}
         {!history.location.pathname.startsWith("/chat") &&
           !history.location.pathname.startsWith("/home") &&
